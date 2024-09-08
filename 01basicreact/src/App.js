c4ae8167fee454507ecb506b40626593bd1bc2ca
Dhruv Soni <sonidhruv30@yahoo.com>
2024-09-08 16:05:57 -0400

    create react projects

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+ return (
+  <h1>char aur code | Dhruv Soni</h1>
+ );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>char aur code with vite | Dhruv Soni</h1>
+  )
+}
+
+export default App
