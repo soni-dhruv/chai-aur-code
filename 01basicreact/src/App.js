9d912cfe10bdbbd98f70ef12b958307de1ad572e
Dhruv Soni <sonidhruv30@yahoo.com>
2024-09-10 13:06:38 -0400

    Understand the React flow and Structure completed

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,7 +1,11 @@
+import { Chai } from "./Chai";
 function App() {
- return (
-  <h1>char aur code | Dhruv Soni</h1>
- );
+    return (
+        <>
+            <Chai />
+            <h1>chai aur react</h1>
+        </>
+    );
 }
 
 export default App;
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,12 @@
-
+import { Chai } from "./chai"
 
 function App() {
   return (
-    <h1>char aur code with vite | Dhruv Soni</h1>
+    <>
+      <Chai />
+      <h1>chai aur react</h1>
+      <p>test para</p>
+    </>
   )
 }
 
