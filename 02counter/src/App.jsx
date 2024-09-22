c34e6d0da7282e17834050a0056ebe11c2fdc2d3
Dhruv Soni <sonidhruv30@yahoo.com>
2024-09-22 12:58:19 -0400

    Completed Hooks and Counter Project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1)
+    }
+  }
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1)
+    }
+  }
+
+  const resetValue = () => {
+    counter = 0
+    setCounter(counter)
+  }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button
+        onClick={addValue}
+      >Add Value: {counter}
+      </button>
+
+      <br />
+
+      <button
+        onClick={removeValue}
+      >Remove Value: {counter}
+      </button>
+
+      <br />
+
+      <button
+        onClick={resetValue}
+      >Reset Value to 0
+      </button>
+
+    </>
+  )
+}
+
+export default App
