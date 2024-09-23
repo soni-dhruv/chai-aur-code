2651e8597ef7586537f2263484d8c8ed16cb7f5e
Dhruv Soni <sonidhruv30@yahoo.com>
2024-09-23 15:43:35 -0400

    Completed learning of Props using TailwindCSS

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Card } from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Test</h1>
+      <Card username='chaiaurcode' btnText='clickme' />
+    </>
+  )
+}
+
+export default App
